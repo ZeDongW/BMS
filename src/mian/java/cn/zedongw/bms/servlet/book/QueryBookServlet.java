02fab7d9fe1537251419be793788f99c9c4e6e80
ZeDongW <878201217@qq.com>
2020-03-29 20:07:05 +0800

    优化代码

--- src/mian/java/cn/zedongw/bms/servlet/book/QueryBookServlet.java
+++ src/mian/java/cn/zedongw/bms/servlet/book/QueryBookServlet.java
@@ -61,6 +61,6 @@ public class QueryBookServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        doGet(req, resp);
     }
 }
