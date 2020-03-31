f8d710044876bb59b7f9e3032d9b45e4a4bee3c0
ZeDongW <878201217@qq.com>
2020-03-31 12:06:24 +0800

    修复结构错误
    Signed-off-by: ZeDongW <878201217@qq.com>

--- src/main/java/cn/zedongw/bms/dao/IBooksDao.java
+++ src/main/java/cn/zedongw/bms/dao/IBooksDao.java
@@ -17,55 +17,60 @@ public interface IBooksDao {
 
     /**
      * 功能描述: 新增书本
-     * @methodName: add
+     *
      * @param book 1
      * @throws
+     * @methodName: add
      * @return: void
      * @author: ZeDongW
      * @date: 2020/3/28 0028 19:50
      */
-    public void add(Books book);
+    void add(Books book);
 
     /**
      * 功能描述: 根据ID删除书本
-     * @methodName: delete
+     *
      * @param id 1
      * @throws
+     * @methodName: delete
      * @return: void
      * @author: ZeDongW
      * @date: 2020/3/28 0028 19:50
      */
-    public void delete(String id);
+    void delete(String id);
 
     /**
      * 功能描述: 更新书本
-     * @methodName: update
+     *
      * @param book 1
      * @throws
+     * @methodName: update
      * @return: void
      * @author: ZeDongW
      * @date: 2020/3/28 0028 19:51
      */
-    public void update(Books book);
+    void update(Books book);
 
     /**
      * 功能描述: 查找所有书本
-     * @methodName: findAll
+     *
      * @throws
+     * @methodName: findAll
      * @return: java.util.ArrayList<cn.zedongw.bms.entity.Books>
      * @author: ZeDongW
      * @date: 2020/3/28 0028 19:51
      */
-    public ArrayList<Books> findAll();
+    ArrayList<Books> findAll();
 
     /**
      * 功能描述: 根据ID查找书本
-     * @methodName: findById
+     *
      * @param id 1
      * @throws
+     * @methodName: findById
      * @return: cn.zedongw.bms.entity.Books
      * @author: ZeDongW
      * @date: 2020/3/28 0028 19:51
      */
-    public Books findById(String id);
+    Books findById(String id);
 }
